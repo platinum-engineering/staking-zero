e6c0024a63e0cdae99dd50d6b1eb4b40cd48d9d5
Dmitriy Gogolev <gogolevdms@gmail.com>
2021-08-19 00:47:55 +0300

    remove proxy, add factory

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -1,18 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.6;
 
-import "./StakingStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Staking is StakingStorageV1 {
+contract Staking is Ownable {
+    address public token;
 
-    function initialize(
-        address token_
-    )
-        public
-    {
+    constructor(address token_) {
         require(
             token == address(0)
-            , "Staking::initialize: may only be initialized once"
+            , "Staking::constructor: may only be initialized once"
         );
 
         token = token_;
---
+++ contracts/StakingPoolFactory.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.6;
+
+import "./Staking.sol";
+
+contract StakingPoolFactory {
+    /**
+     * Fired on creation new staking contract
+     * @param newStakingPool Address of new staking contract
+     */
+    event StakingPoolCreated(address newStakingPool);
+
+    constructor() {}
+
+    /**
+     * Creates new staking contract
+     * @param owner_ The owner of the staking contract
+     * @param stakeToken_ The address of the staking asset
+     */
+    function createStakingPool(address owner_, address stakeToken_) external returns (bool) {
+        Staking newPool = new Staking(stakeToken_);
+
+        newPool.transferOwnership(owner_);
+
+        emit StakingPoolCreated(address(newPool));
+
+        return true;
+    }
+}
