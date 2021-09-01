0d8bf6271e9c37ce64b19693a08cbf1d73b94c7c
Dmitriy Gogolev <gogolevdms@gmail.com>
2021-09-01 23:07:00 +0300

    add tests

--- test/01_StakingPoolFactory.js
+++ test/01_StakingPoolFactory.js
@@ -19,11 +19,12 @@ describe('Staking pool factory', async () => {
     let stakeToken, StakeToken;
     let stakingPoolFactory, StakingPoolFactory;
     let implAndTerms, ImplAndTerms;
+    let whitelist, Whitelist;
     let StakingPool;
     let owner;
 
     before(async () => {
-        [StakeToken, StakingPoolFactory, StakingPool, ImplAndTerms] = await getContractFactories();
+        [StakeToken, StakingPoolFactory, StakingPool, ImplAndTerms, Whitelist] = await getContractFactories();
         [owner] = await ethers.getSigners();
     });
 
@@ -31,11 +32,12 @@ describe('Staking pool factory', async () => {
         stakeToken = await StakeToken.deploy(STAKE_TOKEN_AMOUNT, STAKE_TOKEN_NAME, STAKE_TOKEN_SYMBOL);
         stakingPoolFactory = await StakingPoolFactory.deploy();
         implAndTerms = await ImplAndTerms.deploy();
+        whitelist = await Whitelist.deploy();
     });
 
     describe('Transactions', async () => {
         it('deploy new StakingPool and check data', async () => {
-            const tx = await (await stakingPoolFactory.createStakingPool(owner.address, implAndTerms.address, stakeToken.address)).wait();
+            const tx = await (await stakingPoolFactory.createStakingPool(owner.address, implAndTerms.address, whitelist.address, stakeToken.address)).wait();
             const event = tx.events.find(e => e.event === eventsName.StakingPoolCreated);
             const address = event.args[0];
 
@@ -55,17 +57,17 @@ describe('Staking pool factory', async () => {
         });
         
         it('Should fail due to zero owner address', async () => {
-            await expect(stakingPoolFactory.createStakingPool(ethers.constants.AddressZero, implAndTerms.address, stakeToken.address))
+            await expect(stakingPoolFactory.createStakingPool(ethers.constants.AddressZero, implAndTerms.address, whitelist.address, stakeToken.address))
                 .to.be.revertedWith(getAddressIs0ErrorMessage('StakingPoolFactory', 'createStakingPool'));
         });
         
         it('Should fail due to zero implementation address', async () => {
-            await expect(stakingPoolFactory.createStakingPool(owner.address, ethers.constants.AddressZero, stakeToken.address))
+            await expect(stakingPoolFactory.createStakingPool(owner.address, ethers.constants.AddressZero, whitelist.address, stakeToken.address))
                 .to.be.revertedWith(getAddressIs0ErrorMessage('StakingPoolFactory', 'createStakingPool'));
         });
     
         it('Should fail due to zero stake token address', async () => {
-            await expect(stakingPoolFactory.createStakingPool(owner.address, implAndTerms.address, ethers.constants.AddressZero))
+            await expect(stakingPoolFactory.createStakingPool(owner.address, implAndTerms.address, whitelist.address, ethers.constants.AddressZero))
                 .to.be.revertedWith(getAddressIs0ErrorMessage('StakingPoolFactory', 'createStakingPool'));
         });
     });
